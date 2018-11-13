b6c93d49324025fd2f0e972b4ed9a017db2ef1eb
holt-soundboard <parthkabra@Electricity.local>
2018-11-13 15:29:04 +0800

    allow dismiss message

--- js/loading.js
+++ js/loading.js
@@ -2,3 +2,9 @@ window.addEventListener("load", function() {
     let load_screen = document.getElementById("loading");
     document.body.removeChild(loading)
 })
+
+// cross button for message
+let message = document.getElementById("message");
+message.onclick = function() {
+    message.style.display = "none";
+}
