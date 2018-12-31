2cea6497a12fc9ab03738b05cd18688fbd028faa
holt-soundboard <parthkabra@Electricity.local>
2018-12-31 17:53:07 +0800

    nicer updates

---
+++ assets/data_structure.py
@@ -0,0 +1,24 @@
+import os
+import glob
+from pathlib import Path
+from pprint import pprint
+
+p = Path("/Users/parthkabra/Desktop/Web/HOLT_SOUNDBOARD_5/holt-soundboard-pro/src/assets/sounds")
+sounds = {}
+
+
+for i in p.glob('**/*.mp3'):
+  sound_path = str(i).split("sounds/")[1]
+  print(sound_path)
+
+  # make sure not system file
+  if not (sound_path[0] == "."):
+    character, filename = sound_path.split('/')[0], sound_path.split('/')[1]
+
+    try: sounds[character].append(filename)
+    except: sounds[character] = [filename]
+
+for char, list in sounds.items():
+  sounds[char].sort()
+
+print(sounds)
